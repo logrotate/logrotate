07cd2ac2ca7dbab7b708b2488a8950cfe3172d6f
ewt <ewt@ec1272ba-9ed1-42ef-8245-99669996828e>
1996-10-02 20:53:51 +0000

    changed LOG_ contants to MESS_ constants added logSetErrorFile(), logSetMessageFile()

--- log.c
+++ log.c
@@ -1,34 +1,70 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #include "log.h"
 
-int logLevel = LOG_DEBUG;
+int logLevel = MESS_NORMAL;
+static FILE * errorFile = stderr;
+static FILE * messageFile = stderr;
+
+void logSetErrorFile(FILE * f) {
+    errorFile = f;
+}
+
+void logSetMessageFile(FILE * f) {
+    messageFile = f;
+}
+
+void log(int fd, char * format, ...) {
+    int i = 0;
+    char * buf = NULL;
+    va_list args;
+    int size = -1;
+
+    va_start(args, format);
+
+    do {
+	i += 1000;
+	if (buf) free(buf);
+	buf = malloc(i);
+	size = vsnprintf(buf, size, format, args);
+    } while (i == size);
+
+    write(fd, buf, size);
+
+    free(buf);
+
+    va_end(args);
+}
 
 void message(int level, char * format, ...) {
     va_list args;
-    FILE * where = stderr;
+    FILE * where = errorFile;
 
     if (level >= logLevel) {
 	va_start(args, format);
 
 	switch (level) {
-	    case LOG_DEBUG:
-		where = stdout;
+	    case MESS_DEBUG:
+		where = messageFile;
 		break;
 
-	    case LOG_NORMAL:
-	    case LOG_VERBOSE:
-		where = stdout;
+	    case MESS_NORMAL:
+	    case MESS_VERBOSE:
+		where = messageFile;
 		break;
 
 	    default:
-		fprintf(stderr, "error: ");
+		fprintf(errorFile, "error: ");
 	}
 
 	vfprintf(where, format, args);
 
-	if (level == LOG_FATAL) exit(1);
+	va_end(args);
+
+	if (level == MESS_FATAL) exit(1);
     }
 }
 
--- log.h
+++ log.h
@@ -1,13 +1,18 @@
 #ifndef H_LOG
 #define H_LOG
 
-#define LOG_REALDEBUG	1
-#define LOG_DEBUG	2
-#define LOG_VERBOSE	3
-#define LOG_NORMAL	4
-#define LOG_ERROR	5
-#define LOG_FATAL	6
+#include <stdio.h>
+
+#define MESS_REALDEBUG	1
+#define MESS_DEBUG	2
+#define MESS_VERBOSE	3
+#define MESS_NORMAL	4
+#define MESS_ERROR	5
+#define MESS_FATAL	6
 
 void message(int level, char * format, ...);
+void log(int fd, char * format, ...);
+void logSetErrorFile(FILE * f);
+void logSetMessageFile(FILE * f);
 
 #endif
