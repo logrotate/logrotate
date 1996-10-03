ab65a89f174951265f3a6bfb17ecb67272d087e4
ewt <ewt@ec1272ba-9ed1-42ef-8245-99669996828e>
1996-10-03 22:12:41 +0000

    1) made DEBUG the normal debugging 2) added timestamp to debug messages

--- log.c
+++ log.c
@@ -1,11 +1,12 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 #include <unistd.h>
 
 #include "log.h"
 
-int logLevel = MESS_NORMAL;
+int logLevel = MESS_DEBUG;
 static FILE * errorFile = stderr;
 static FILE * messageFile = stderr;
 
@@ -42,6 +43,7 @@ void log(int fd, char * format, ...) {
 void message(int level, char * format, ...) {
     va_list args;
     FILE * where = errorFile;
+    int showTime = 0;
 
     if (level >= logLevel) {
 	va_start(args, format);
@@ -49,6 +51,7 @@ void message(int level, char * format, ...) {
 	switch (level) {
 	    case MESS_DEBUG:
 		where = messageFile;
+		showTime = 1;
 		break;
 
 	    case MESS_NORMAL:
@@ -58,6 +61,11 @@ void message(int level, char * format, ...) {
 
 	    default:
 		fprintf(errorFile, "error: ");
+		showTime = 1;
+	}
+
+	if (showTime) {
+	    fprintf(where, "%ld:", (long) time(NULL));
 	}
 
 	vfprintf(where, format, args);
