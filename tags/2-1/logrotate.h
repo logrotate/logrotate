0510bd512f571e28f68941a6e69b35e0c8fa0332
nobody <nobody@ec1272ba-9ed1-42ef-8245-99669996828e>
1997-01-13 16:35:40 +0000

    This commit was manufactured by cvs2svn to create tag '2-1'.

---
+++ tags/2-1/logrotate.h
@@ -0,0 +1,34 @@
+#ifndef H_LOGROTATE
+#define H_LOGROTATE
+
+#include <sys/types.h>
+
+#define LOG_FLAG_COMPRESS	(1 << 0)
+#define LOG_FLAG_CREATE		(1 << 1)
+
+#define COMPRESS_COMMAND "gzip -9"
+#define COMPRESS_EXT ".gz"
+#define UNCOMPRESS_PIPE "gunzip"
+
+#define STATEFILE "/var/lib/logrotate.status"
+
+typedef struct {
+    char * fn;
+    enum { ROT_DAYS, ROT_WEEKLY, ROT_MONTHLY, ROT_SIZE } criterium;
+    unsigned int threshhold;
+    int rotateCount;
+    char * pre, * post;
+    char * logAddress;
+    char * errAddress;
+    int flags;
+    mode_t createMode;		/* if any/all of these are -1, we use the */
+    uid_t createUid;		/* attributes from the log file just rotated */
+    gid_t createGid;
+} logInfo;
+
+int readConfigPath(char * path, logInfo * defConfig, 
+			  logInfo ** logsPtr, int * numLogsPtr);
+
+extern int debug;
+
+#endif
