5828200cfc445cf9a77bd7b05fa68cb75d1fc4ea
nobody <nobody@ec1272ba-9ed1-42ef-8245-99669996828e>
1997-09-01 20:15:20 +0000

    This commit was manufactured by cvs2svn to create tag 'r2-5'.

---
+++ tags/r2-5/log.c
@@ -0,0 +1,95 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#include "log.h"
+
+int logLevel = MESS_DEBUG;
+static FILE * errorFile = stderr;
+static FILE * messageFile = stderr;
+int flags = 0;
+
+void logSetLevel(int level) {
+    logLevel = level;
+}
+
+void logSetErrorFile(FILE * f) {
+    errorFile = f;
+}
+
+void logSetMessageFile(FILE * f) {
+    messageFile = f;
+}
+
+void logSetFlags(int newFlags) {
+    flags |= newFlags;
+}
+
+void logClearFlags(int newFlags) {
+    flags &= ~newFlags;
+}
+
+void log(int fd, char * format, ...) {
+    int i = 0;
+    char * buf = NULL;
+    va_list args;
+    int size = -1;
+
+    va_start(args, format);
+
+    do {
+	i += 1000;
+	if (buf) free(buf);
+	buf = malloc(i);
+	size = vsnprintf(buf, size, format, args);
+    } while (i == size);
+
+    write(fd, buf, size);
+
+    free(buf);
+
+    va_end(args);
+}
+
+void message(int level, char * format, ...) {
+    va_list args;
+    FILE * where = errorFile;
+    int showTime = 0;
+
+    if (level >= logLevel) {
+	va_start(args, format);
+
+	switch (level) {
+	  case MESS_DEBUG:
+	    where = messageFile;
+	    showTime = 1;
+	    break;
+
+	  case MESS_NORMAL:
+	  case MESS_VERBOSE:
+	    where = messageFile;
+	    break;
+
+	  default:
+	    if (flags & LOG_TIMES) 
+		fprintf(where, "%ld: ", (long) time(NULL));
+	    fprintf(errorFile, "error: ");
+	    break;
+	}
+
+	if (showTime && (flags & LOG_TIMES)) {
+	    fprintf(where, "%ld:", (long) time(NULL));
+	}
+
+	vfprintf(where, format, args);
+	fflush(where);
+
+	va_end(args);
+
+	if (level == MESS_FATAL) exit(1);
+    }
+}
+
+
---
+++ tags/r2-5/logrotate.spec
@@ -0,0 +1,38 @@
+Summary: Rotates, compresses, and mails system logs
+Name: logrotate
+Version: 2.5
+Release: 1
+Copyright: GPL
+Group: Utilities/System
+Source: ftp://ftp.redhat.com/pub/redhat/code/logrotate/logrotate-2.4.tar.gz
+BuildRoot: /var/tmp/logrotate.root
+
+%description
+Logrotate is designed to ease administration of systems that generate
+large numbers of log files. It allows automatic rotation, compression,
+removal, and mailing of log files. Each log file may be handled daily,
+weekly, monthly, or when it grows too large.
+
+%prep
+%setup
+
+%build
+make RPM_OPT_FLAGS="$RPM_OPT_FLAGS"
+
+%install
+make PREFIX=$RPM_BUILD_ROOT install
+mkdir -p $RPM_BUILD_ROOT/etc/logrotate.d
+mkdir -p $RPM_BUILD_ROOT/etc/cron.daily
+
+install -m 644 examples/logrotate-default $RPM_BUILD_ROOT/etc/logrotate.conf
+install -m 755 examples/logrotate.cron $RPM_BUILD_ROOT/etc/cron.daily/logrotate
+
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+%files
+%attr(0755, root, root) /usr/sbin/logrotate
+%attr(0644, root, root) /usr/man/man8/logrotate.8
+%attr(0755, root, root) /etc/cron.daily/logrotate
+%attr(0644, root, root) %config /etc/logrotate.conf
+%attr(0755, root, root) %dir /etc/logrotate.d
