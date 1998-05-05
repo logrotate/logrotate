b830ab10b33b94a72b1cf5ac2a27db1d87c3df40
ewt <ewt@ec1272ba-9ed1-42ef-8245-99669996828e>
1998-05-05 02:17:47 +0000

    - added nodelaycompress flag (from Jos Vos) - added copytruncate, nocopytruncate flag (from Jos Vos) - removed umask handling; explicitly use fchmod() insteadmoved umask - added --force option (Simon Mudd) - moved /bin/mail to MAIL_COMMAND define (Simon Mudd) - fixed segv caused by overly long filenames - switched from getopt_long to popt - version 2.6

--- logrotate.h
+++ logrotate.h
@@ -8,10 +8,15 @@
 #define LOG_FLAG_CREATE		(1 << 1)
 #define LOG_FLAG_IFEMPTY	(1 << 2)
 #define LOG_FLAG_DELAYCOMPRESS	(1 << 3)
+#define LOG_FLAG_COPYTRUNCATE	(1 << 4)
 
+#define MAIL_COMMAND "/bin/mail -s"
 #define COMPRESS_COMMAND "gzip -9"
 #define COMPRESS_EXT ".gz"
 #define UNCOMPRESS_PIPE "gunzip"
+
+#define NO_FORCE_ROTATE 0
+#define FORCE_ROTATE    1
 
 #define STATEFILE "/var/lib/logrotate.status"
 
@@ -20,7 +25,7 @@ typedef struct {
     char ** files;
     int numFiles;
     char * oldDir;
-    enum { ROT_DAYS, ROT_WEEKLY, ROT_MONTHLY, ROT_SIZE } criterium;
+    enum { ROT_DAYS, ROT_WEEKLY, ROT_MONTHLY, ROT_SIZE, ROT_FORCE } criterium;
     unsigned int threshhold;
     int rotateCount;
     char * pre, * post;
