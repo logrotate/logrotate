ddc3fff8a7f9f9b581eee9d55ee47cf46a4adcae
ewt <ewt@ec1272ba-9ed1-42ef-8245-99669996828e>
1999-03-05 13:53:08 +0000

    1) wasn't handling missing files properly 2) wasn't handling empty globs properly

--- trunk/config.c
+++ trunk/config.c
@@ -674,7 +674,7 @@ static int readConfigFile(char * configFile, logInfo * defConfig,
 	    oldchar = *endtag;
 	    *endtag = '\0';
 
-	    rc = glob(start, 0, globerr, &globResult);
+	    rc = glob(start, GLOB_NOCHECK, globerr, &globResult);
 	    if (rc == GLOB_ABORTED) return 1;
 
 	    newlog->files = malloc(sizeof(*newlog->files) * 
@@ -683,8 +683,8 @@ static int readConfigFile(char * configFile, logInfo * defConfig,
 
 	    for (i = 0; i < globResult.gl_pathc; i++) {
 		/* if we glob directories we can get false matches */
-		if (lstat(globResult.gl_pathv[i], &sb)) continue;
-		if (S_ISDIR(sb.st_mode)) continue;
+		if (!lstat(globResult.gl_pathv[i], &sb) && S_ISDIR(sb.st_mode)) 
+		    continue;
 
 		for (j = 0; j < *numLogsPtr - 1; j++) {
 		    for (k = 0; k < (*logsPtr)[j].numFiles; k++) {
