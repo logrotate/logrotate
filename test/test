logrotate/logrotate

commit 3b26f4d8bbb338981aa2796c4076792c63d850c0

a fix for rotating (copying) non-writable, readable files (rhbz#545919)

logrotate.c
@@ -436,7 +436,7 @@ static int copyTruncate(char *currLog, char *saveLog, struct stat *sb,
     message(MESS_DEBUG, "copying %s to %s\n", currLog, saveLog);
 
     if (!debug) {
-	if ((fdcurr = open(currLog, O_RDWR)) < 0) {
+	if ((fdcurr = open(currLog, (flags & LOG_FLAG_COPY) ? O_RDONLY : O_RDWR)) < 0) {
 	    message(MESS_ERROR, "error opening %s: %s\n", currLog,
 		    strerror(errno));
 	    return 1;

commit 65faf212e51115699f55f491d464f0ead9c2047e

make 'copy' and 'copytruncate' work together
This commit fixes a regression introduced
by 3b26f4d8bbb338981aa2796c4076792c63d850c0.

logrotate.c
@@ -1019,7 +1019,10 @@ static int copyTruncate(char *currLog, char *saveLog, struct stat *sb,
     message(MESS_DEBUG, "copying %s to %s\n", currLog, saveLog);
 
     if (!debug) {
-	if ((fdcurr = open(currLog, ((flags & LOG_FLAG_COPY) ? O_RDONLY : O_RDWR) | O_NOFOLLOW)) < 0) {
+	/* read access is sufficient for 'copy' but not for 'copytruncate' */
+	const int read_only = (flags & LOG_FLAG_COPY)
+	    && !(flags & LOG_FLAG_COPYTRUNCATE);
+	if ((fdcurr = open(currLog, ((read_only) ? O_RDONLY : O_RDWR) | O_NOFOLLOW)) < 0) {
 	    message(MESS_ERROR, "error opening %s: %s\n", currLog,
 		    strerror(errno));
 	    return 1;

test/test
@@ -1842,6 +1842,19 @@ test.log.2.gz 1 zero
 test.log.3.gz 1 first
 EOF
 
+cleanup 73
+
+# ------------------------------- Test 73 ------------------------------------
+# make sure that 'copy' and 'copytruncate' work together
+preptest test.log 73 2
+
+$RLR test-config.73 --force
+
+checkoutput <<EOF
+test.log 0
+test.log.1 0 zero
+EOF
+
 cleanup 100
 
 # ------------------------------- Test 100 ------------------------------------

test/test-config.73.in
@@ -1,0 +1,4 @@
+&DIR&/test*.log {
+    copy
+    copytruncate
+}