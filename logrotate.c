b79c47544259fec58efa10af6d75f2fd4c6a26c2
Christian Göttsche <cgzones@googlemail.com>
2017-09-19 20:03:33 +0200

    add compile time settings into version output
    example output:
    
    $ logrotate --version
    logrotate 3.12.3
    
        Default mail command:       /bin/mail
        Default compress command:   /bin/gzip
        Default uncompress command: /bin/gunzip
        Default compress extension: .gz
        Default state file path:    /var/lib/logrotate.status
        ACL support:                yes
        SELinux support:            yes

--- logrotate.c
+++ logrotate.c
@@ -2600,6 +2600,22 @@ int main(int argc, const char **argv)
 		break;
 	case 'V':
 	    fprintf(stderr, "logrotate %s\n", VERSION);
+	    fprintf(stderr, "\n");
+	    fprintf(stderr, "    Default mail command:       %s\n", DEFAULT_MAIL_COMMAND);
+	    fprintf(stderr, "    Default compress command:   %s\n", COMPRESS_COMMAND);
+	    fprintf(stderr, "    Default uncompress command: %s\n", UNCOMPRESS_COMMAND);
+	    fprintf(stderr, "    Default compress extension: %s\n", COMPRESS_EXT);
+	    fprintf(stderr, "    Default state file path:    %s\n", STATEFILE);
+#ifdef WITH_ACL
+	    fprintf(stderr, "    ACL support:                yes\n");
+#else
+	    fprintf(stderr, "    ACL support:                no\n");
+#endif
+#ifdef WITH_SELINUX
+	    fprintf(stderr, "    SELinux support:            yes\n");
+#else
+	    fprintf(stderr, "    SELinux support:            no\n");
+#endif
 	    poptFreeContext(optCon);
 	    exit(0);
 	default:
