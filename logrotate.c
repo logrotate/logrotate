0d805cee34d3a9b1937130fb7c7fcf75d940f51f
Craig Treleaven <ctreleaven@cogeco.ca>
2019-02-10 06:11:27 -0800

    logrotate: do not use utimensat() on Darwin
    ... to prevent the following build failure:
    
    /usr/bin/clang -DHAVE_CONFIG_H -I.  -include config.h -I/opt/local/include -Wall -Wextra -Wmissing-format-attribute -Wmissing-noreturn -Wpointer-arith -Wshadow -Wstrict-prototypes -Wundef -Wunused -Wwrite-strings -pipe -Os -arch x86_64 -MT logrotate.o -MD -MP -MF .deps/logrotate.Tpo -c -o logrotate.o logrotate.c
    logrotate.c:478:38: warning: unused parameter 'acl' [-Wunused-parameter]
                                acl_type acl, int force_mode)
                                         ^
    logrotate.c:694:17: error: no member named 'st_atim' in 'struct stat'
        ts[0] = sb->st_atim;
                ~~  ^
    logrotate.c:695:17: error: no member named 'st_mtim' in 'struct stat'
        ts[1] = sb->st_mtim;
                ~~  ^
    1 warning and 2 errors generated.
    
    Fixes #241

--- logrotate.c
+++ logrotate.c
@@ -688,7 +688,7 @@ static void setAtimeMtime(const char *filename, const struct stat *sb)
     /* If we can't change atime/mtime, it's not a disaster.  It might
        possibly fail under SELinux. But do try to preserve the
        fractional part if we have utimensat(). */
-#ifdef HAVE_UTIMENSAT
+#if defined HAVE_UTIMENSAT && !defined(__APPLE__)
     struct timespec ts[2];
 
     ts[0] = sb->st_atim;
