logrotate/logrotate

commit dfe8dc4688254bd33d30bb4f976316f12e088f55

fix gcc -Wstringop-overflow warnings
logrotate.c: In function ‘prerotateSingleLog’:
logrotate.c:1485:13: warning: ‘strncat’ specified bound depends on the length of the source argument [-Wstringop-overflow=]
 1485 |             strncat(tempstr, rotNames->baseName, baseLen - extLen);
      |             ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
logrotate.c:1477:26: note: length computed here
 1477 |         size_t baseLen = strlen(rotNames->baseName);
      |                          ^~~~~~~~~~~~~~~~~~~~~~~~~~
logrotate.c:1504:9: warning: ‘strncat’ specified bound depends on the length of the source argument [-Wstringop-overflow=]
 1504 |         strncat(tempstr, rotNames->baseName,
      |         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 1505 |                 strlen(rotNames->baseName) - strlen(log->extension));
      |                 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
logrotate.c:1505:17: note: length computed here
 1505 |                 strlen(rotNames->baseName) - strlen(log->extension));
      |                 ^~~~~~~~~~~~~~~~~~~~~~~~~~

Fixes #263

logrotate.c
@@ -1479,10 +1479,9 @@ static int prerotateSingleLog(struct logInfo *log, int logNum,
         if (baseLen >= extLen &&
                 strncmp(&(rotNames->baseName[baseLen - extLen]),
                     log->addextension, extLen) == 0) {
-            char *tempstr;
 
-            tempstr = calloc(baseLen - extLen + 1, sizeof(char));
-            strncat(tempstr, rotNames->baseName, baseLen - extLen);
+            char *tempstr = strndup(rotNames->baseName, baseLen - extLen);
+
             free(rotNames->baseName);
             rotNames->baseName = tempstr;
         }
@@ -1498,12 +1497,7 @@ static int prerotateSingleLog(struct logInfo *log, int logNum,
         char *tempstr;
 
         fileext = log->extension;
-        tempstr =
-            calloc(strlen(rotNames->baseName) - strlen(log->extension) + 1,
-                   sizeof(char));
-        strncat(tempstr, rotNames->baseName,
-                strlen(rotNames->baseName) - strlen(log->extension));
-        free(rotNames->baseName);
+        tempstr = strndup(rotNames->baseName, strlen(rotNames->baseName) - strlen(log->extension));
         rotNames->baseName = tempstr;
     }
 

commit 3bd3bcc41677a784c29fc4cb9f350acb92bac702

prerotateSingleLog: fix memory leak
introduced by dfe8dc4688254bd33d30bb4f976316f12e088f55

logrotate.c
@@ -1501,6 +1501,7 @@ static int prerotateSingleLog(struct logInfo *log, int logNum,
 
         fileext = log->extension;
         tempstr = strndup(rotNames->baseName, strlen(rotNames->baseName) - strlen(log->extension));
+        free(rotNames->baseName);
         rotNames->baseName = tempstr;
     }
 