b4e72abf1bd24948c433aee30b272aac95643518
Christian Göttsche <cgzones@googlemail.com>
2019-10-11 15:22:56 +0200

    readConfigFile: increase severity level of unknown switch statement to fatal

--- config.c
+++ config.c
@@ -2007,7 +2007,7 @@ duperror:
                 }
                 break;
             default:
-                message(MESS_DEBUG,
+                message(MESS_FATAL,
                         "%s: %d: readConfigFile() unknown state\n",
                         configFile, lineNum);
         }
