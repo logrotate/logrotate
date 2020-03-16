logrotate/logrotate

commit a56a4f04b2351725ede76dead56af36c2fb2ac14

createOutputFile: suppress a warning without libacl
Signed-off-by: Sami Kerola <kerolasa@iki.fi>

logrotate.c
@@ -542,6 +542,8 @@ static int createOutputFile(char *fileName, int flags, struct stat *sb,
 			acl_set = 1;
 		}
 	}
+#else
+	(void) acl_set;
 #endif
 
 	if (!acl_set || force_mode) {

commit e523de24cbfabeade3d37b8b562aab356f07a7ba

silence correct unused parameter warning in acl disabled build
Fixes: a56a4f04b2351725ede76dead56af36c2fb2ac14 ("createOutputFile: suppress a warning without libacl")

logrotate.c
@@ -588,7 +588,7 @@ static int createOutputFile(const char *fileName, int flags, const struct stat *
         }
     }
 #else
-    (void) acl_set;
+    (void) acl;
 #endif
 
     if (!acl_set || force_mode) {