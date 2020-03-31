8df8805b85d2ec36daec80b02e2c4d2b83b293ea
Christian Göttsche <cgzones@googlemail.com>
2020-03-31 17:00:35 +0200

    fix misspellings

--- logrotate.c
+++ logrotate.c
@@ -895,7 +895,7 @@ static int mailLog(const struct logInfo *log, const char *logFile, const char *m
     }
 
     if (uncompressCommand) {
-        /* pipe used to capture ouput of the uncompress process */
+        /* pipe used to capture output of the uncompress process */
         if (pipe(uncompressPipe) < 0) {
             message(MESS_ERROR, "error opening pipe for uncompress: %s",
                     strerror(errno));
