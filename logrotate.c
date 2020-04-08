logrotate/logrotate

commit a0b05e42a590efa3e575dd2001b6aa390a79c769

switch_user_permanently: check if switchback is possible
See https://wiki.sei.cmu.edu/confluence/display/c/POS37-C.+Ensure+that+privilege+relinquishment+is+successful

Closes #280

logrotate.c
@@ -186,6 +186,12 @@ static int switch_user_permanently(const struct logInfo *log) {
                 (unsigned) user, (unsigned) group, strerror(errno));
         return 1;
     }
+
+    if (setuid(ROOT_UID) != -1) {
+        message(MESS_ERROR, "failed to switch user permanently, able to switch back\n");
+        return 1;
+    }
+
     return 0;
 }
 

commit bf18aec66a2a8bc0c3ef56c6be41846076c8a3f1

switch_user_permanently: skip switchback check if switched to root
Allow switching only the real group (not the user) with a configuration
like `su root somegroup`.
E.g. mailman uses `su root list`, which currently fails with:
    error: failed to switch user permanently, able to switch back
    error: failed to compress log /var/log/mailman/qrunner.1

Fixes: a0b05e42a590efa3e575dd2001b6aa390a79c769 ("switch_user_permanently: check if switchback is possible")

logrotate.c
@@ -183,7 +183,7 @@ static int switch_user_permanently(const struct logInfo *log) {
         return 1;
     }
 
-    if (setuid(ROOT_UID) != -1) {
+    if (user != ROOT_UID && setuid(ROOT_UID) != -1) {
         message(MESS_ERROR, "failed to switch user permanently, able to switch back\n");
         return 1;
     }