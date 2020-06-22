ddc3caab2f7317cacb48db33c503d5deca1f9f79
Christian Göttsche <cgzones@googlemail.com>
2020-06-22 22:06:26 +0200

    Add check that copytruncate and rotate 0 does not keep a rotated log file
    The bug this test covers was fixed in logrotate-3.15.
    
    Closes: https://github.com/logrotate/logrotate/pull/334

--- test/test-0073.sh
+++ test/test-0073.sh
@@ -6,11 +6,19 @@ cleanup 73
 
 # ------------------------------- Test 73 ------------------------------------
 # make sure that 'copy' and 'copytruncate' work together
-preptest test.log 73 2
+preptest test_copy.log 73 2
+# make sure that 'rotate 0' and 'copytruncate' work together
+preptest test_rotate.log 73 0
 
 $RLR test-config.73 --force
 
 checkoutput <<EOF
-test.log 0
-test.log.1 0 zero
+test_copy.log 0
+test_copy.log.1 0 zero
+test_rotate.log 0
 EOF
+
+if [ -f test_rotate.log.1 ]; then
+    echo "rotate 0 and copytruncate keep a rotated log file"
+    exit 3
+fi
