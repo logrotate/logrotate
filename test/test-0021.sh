1e1c1f29556db53288761d435dc850c818b2637a
Christian Göttsche <cgzones@googlemail.com>
2021-06-02 16:24:18 +0200

    tests: enforce test 21
    Actually check that the file does not get rotated.

--- test/test-0021.sh
+++ test/test-0021.sh
@@ -7,12 +7,25 @@ cleanup 21
 # ------------------------------- Test 21 ------------------------------------
 # different base name, so it should not find the file
 preptest differenttest.log 21 1
+
+checkoutput <<EOF
+differenttest.log 0 zero
+differenttest.log.1 0 first
+EOF
+
 $RLR test-config.21 --force 2>error.log || exit 23
 
-cat error.log
+checkoutput <<EOF
+differenttest.log 0 zero
+differenttest.log.1 0 first
+EOF
 
-# grep "error running shared postrotate script for" error.log >/dev/null
-# if [ $? != 0 ]; then
-# 	echo "No error printed, but there should be one."
-# 	exit 3
-# fi
+if [ -e test.log ]; then
+	echo "test-log exists"
+	exit 3
+fi
+
+if [ -e test.log.1 ]; then
+	echo "test.log.1 exists"
+	exit 3
+fi
