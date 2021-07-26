logrotate/logrotate

commit 2b588b5ec2e5c27bee857c4abeddafa6a9602ebc

Log if keyword is not properly separated
The man page states
  Values are separated from directives by whitespace and/or an
  optional =.

But logrotate does accept no separator, like
  rotate7

Log those occurrences with a normal severity, as this usage is not
intended.

config.c
@@ -1095,6 +1095,11 @@ static int readConfigFile(const char *configFile, struct logInfo *defConfig)
                     key = isolateWord(&start, &buf, length);
                     if (key == NULL)
                         continue;
+                    if (!isspace((unsigned char)*start)) {
+                        message(MESS_NORMAL, "%s:%d keyword '%s' not properly"
+                                " separated, found %#x\n",
+                                configFile, lineNum, key, *start);
+                    }
                     if (!strcmp(key, "compress")) {
                         newlog->flags |= LOG_FLAG_COMPRESS;
                     } else if (!strcmp(key, "nocompress")) {

commit 1300c45840c790d0d7827efb30152fd504daffba

Do not warn on key value pair separated by only an equal sign
Do not warn if a configuration directive is specified with the key and
value separated by just an equal sign, like:

    size=+2048k

The warning is intended for the usage of:

    size2048k

Fixes: 2b588b5e ("Log if keyword is not properly separated")
Fixes: #410

config.c
@@ -1109,7 +1109,7 @@ static int readConfigFile(const char *configFile, struct logInfo *defConfig)
                                 configFile, lineNum);
                         continue;
                     }
-                    if (!isspace((unsigned char)*start)) {
+                    if (!isspace((unsigned char)*start) && *start != '=') {
                         message(MESS_NORMAL, "%s:%d keyword '%s' not properly"
                                 " separated, found %#x\n",
                                 configFile, lineNum, key, *start);