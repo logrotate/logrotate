logrotate/logrotate

commit 4eb3add422f7d50128600eae3c678370fddc2217

Use difftime to compare time_t
On platforms with unsigned time_t simple subtraction may produce a large
positive result instead of the expected negative value.  Use difftime()
to ensure meaningful comparison.

Closes: https://github.com/logrotate/logrotate/pull/438

logrotate.c
@@ -1340,8 +1340,8 @@ static time_t mktimeFromDateOnly(const struct tm *src)
 /* return by how many days the date was advanced but ignore exact time */
 static time_t daysElapsed(const struct tm *now, const struct tm *last)
 {
-    const time_t diff = mktimeFromDateOnly(now) - mktimeFromDateOnly(last);
-    return diff / (24 * 3600);
+    const double diff = difftime(mktimeFromDateOnly(now),mktimeFromDateOnly(last));
+    return (time_t) (diff / (24 * 3600));
 }
 
 static int findNeedRotating(const struct logInfo *log, unsigned logNum, int force)
@@ -1443,7 +1443,7 @@ static int findNeedRotating(const struct logInfo *log, unsigned logNum, int forc
             message(MESS_DEBUG, "  log does not need rotating "
                     "(log size is below the 'size' threshold)\n");
         }
-    } else if (mktime(&state->lastRotated) - mktime(&now) > (25 * 3600)) {
+    } else if (difftime(mktime(&state->lastRotated), mktime(&now)) > (25 * 3600)) {
         /* 25 hours allows for DST changes as well as geographical moves */
         message(MESS_ERROR,
                 "log %s last rotated in the future -- rotation forced\n",
@@ -1532,7 +1532,7 @@ static int findNeedRotating(const struct logInfo *log, unsigned logNum, int forc
                     "('minsize' directive is used and the log "
                     "size is smaller than the minsize value)\n");
         }
-        if (log->rotateMinAge && log->rotateMinAge * DAY_SECONDS >= nowSecs - sb.st_mtime) {
+        if (log->rotateMinAge && log->rotateMinAge * DAY_SECONDS >= difftime(nowSecs, sb.st_mtime)) {
             state->doRotate = 0;
             message(MESS_DEBUG, "  log does not need rotating "
                     "('minage' directive is used and the log "
@@ -1925,7 +1925,7 @@ static int prerotateSingleLog(const struct logInfo *log, unsigned logNum,
                     if (((globResult.gl_pathc >= (size_t)rotateCount) && (glob_count <= (globResult.gl_pathc - (size_t)rotateCount)))
                             || ((log->rotateAge > 0)
                                 &&
-                                (((nowSecs - fst_buf.st_mtime) / DAY_SECONDS)
+                                ((difftime(nowSecs, fst_buf.st_mtime) / DAY_SECONDS)
                                  > log->rotateAge))) {
                         if (mail_out != (size_t)-1) {
                             char *mailFilename =
@@ -2039,7 +2039,7 @@ static int prerotateSingleLog(const struct logInfo *log, unsigned logNum,
                     continue;
                 }
 
-                if (((nowSecs - fst_buf.st_mtime) / DAY_SECONDS) > log->rotateAge) {
+                if ((difftime(nowSecs, fst_buf.st_mtime) / DAY_SECONDS) > log->rotateAge) {
                     if (!hasErrors && log->logAddress)
                         hasErrors = mailLogWrapper(oldName, mailCommand,
                                                    logNum, log);

commit 430da03a38d9e2b1f70b118f65b8161ed718e223

Restore old time_t difference calculations
With the introduction of difftime(3) in commit 4eb3add4 ("Use difftime
to compare time_t") to support platform with an unsigned time_t several
arithmetic operations and comparisons turned from integer to floating
point ones.  This can lead to different results since integer divisions
round down to the next integer while floating point divisions do not.

Cast the result of difftime(3) to intmax_t to revert back to integer
operations.

Fixes: 4eb3add4 ("Use difftime to compare time_t")
Fixes: #514

logrotate.c
@@ -1352,7 +1352,7 @@ static time_t mktimeFromDateOnly(const struct tm *src)
 static long daysElapsed(const struct tm *now, const struct tm *last)
 {
     const double diff = difftime(mktimeFromDateOnly(now), mktimeFromDateOnly(last));
-    return (long) (diff / DAY_SECONDS);
+    return (long) ((intmax_t)diff / DAY_SECONDS);
 }
 
 static int findNeedRotating(const struct logInfo *log, unsigned logNum, int force)
@@ -1935,7 +1935,7 @@ static int prerotateSingleLog(const struct logInfo *log, unsigned logNum,
                     if (((globResult.gl_pathc >= (size_t)rotateCount) && (glob_count <= (globResult.gl_pathc - (size_t)rotateCount)))
                             || ((log->rotateAge > 0)
                                 &&
-                                ((difftime(nowSecs, fst_buf.st_mtime) / DAY_SECONDS)
+                                (((intmax_t)difftime(nowSecs, fst_buf.st_mtime) / DAY_SECONDS)
                                  > log->rotateAge))) {
                         if (mail_out != (size_t)-1) {
                             char *mailFilename =
@@ -2049,7 +2049,7 @@ static int prerotateSingleLog(const struct logInfo *log, unsigned logNum,
                     continue;
                 }
 
-                if ((difftime(nowSecs, fst_buf.st_mtime) / DAY_SECONDS) > log->rotateAge) {
+                if (((intmax_t)difftime(nowSecs, fst_buf.st_mtime) / DAY_SECONDS) > log->rotateAge) {
                     if (!hasErrors && log->logAddress)
                         hasErrors = mailLogWrapper(oldName, mailCommand,
                                                    logNum, log);

test/test-0085.sh
@@ -44,30 +44,6 @@ if [ -f test.log.2 ]; then
 fi
 
 # maxage should remove this file
-if [ -f test.log.5 ]; then
-    echo "test.log.5 not removed" >&2
-    exit 3
-fi
-
-# maxage should remove this file
-if [ -f test.log.6 ]; then
-    echo "test.log.6 not removed" >&2
-    exit 3
-fi
-
-# maxage should remove this file
-if [ -f test.log.7 ]; then
-    echo "test.log.7 not removed" >&2
-    exit 3
-fi
-
-# maxage should remove this file
-if [ -f test.log.8 ]; then
-    echo "test.log.8 not removed" >&2
-    exit 3
-fi
-
-# maxage should remove this file
 if [ -f test.log.9 ]; then
     echo "test.log.9 not removed" >&2
     exit 3
@@ -84,4 +60,8 @@ test.log 0
 test.log.1 0 content
 test.log.3 0 first
 test.log.4 0 second
+test.log.5 0 third
+test.log.6 0 fourth
+test.log.7 0 fifth
+test.log.8 0 sixth
 EOF